bad8e183c771cb660b2fd0a8222c035ccaac9dea
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-24 12:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function manipulateData(inputArray) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  })
+    .then((array) => {
+      const filteredArray = array.filter((num) => num % 2 === 0);
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(filteredArray);
+        }, 1000);
+      });
+    })
+    .then((array) => {
+      const mappedArray = array.map((num) => num * 2);
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(mappedArray);
+        }, 2000);
+      });
+    });
+}
+
+manipulateData([1, 2, 3, 4]).then((result) => {
+  document.getElementById("output").textContent = result.join(", ");
+});
