64609eb19312b421c2c193b2c9288070dcc31eec
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-01 00:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function manipulateArray(arr) {
-  return delay(3000).then(() => {
-    let evenNumbers = arr.filter(num => num % 2 === 0);
-    let oddNumbers = arr.filter(num => num % 2 !== 0);
-
-    return { evenNumbers, oddNumbers };
+function manipulateData(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  })
+  .then((arr) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const filteredArr = arr.filter((num) => num % 2 !== 1);
+        resolve(filteredArr);
+      }, 1000);
+    });
+  })
+  .then((arr) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const mappedArr = arr.map((num) => num * 2);
+        resolve(mappedArr);
+      }, 2000);
+    });
+  })
+  .then((arr) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = arr.join(",");
   });
 }
 
-function filterOddNumbers(arr) {
-  return delay(1000).then(() => arr.filter(num => num % 2 === 0));
-}
-
-function multiplyEvenNumbers(arr) {
-  return delay(2000).then(() => arr.map(num => num * 2));
-}
-
-let inputArr = [1, 2, 3, 4];
-let outputDiv = document.getElementById("output");
-
-manipulateArray(inputArr)
-  .then(result => filterOddNumbers(result.evenNumbers))
-  .then(result => multiplyEvenNumbers(result))
-  .then(result => {
-    outputDiv.textContent = result.join(", ");
-  });
+manipulateData([1, 2, 3, 4]);
