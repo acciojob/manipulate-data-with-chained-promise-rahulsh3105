7f38585cc20de7e244505d44f1cf2946708bf370
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-01 00:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-function manipulateData(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(arr);
-    }, 3000);
-  })
-  .then((arr) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const filteredArr = arr.filter((num) => num % 2 !== 1);
-        resolve(filteredArr);
-      }, 1000);
-    });
-  })
-  .then((arr) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const mappedArr = arr.map((num) => num * 2);
-        resolve(mappedArr);
-      }, 2000);
-    });
-  })
-  .then((arr) => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = arr.join(",");
-  });
-}
+const outputDiv = document.getElementById('output');
 
-manipulateData([1, 2, 3, 4]);
+getNumbers()
+  .then(numbers => {
+    console.log(numbers);
+    return filterOdd(numbers);
+  })
+  .then(evenNumbers => {
+    console.log(evenNumbers);
+    return doubleEven(evenNumbers);
+  })
+  .then(finalNumbers => {
+    console.log(finalNumbers);
+    outputDiv.textContent = finalNumbers.join(',');
+  })
+  .catch(error => {
+    console.error(error);
+  });
