30c5877fb13222a6e8ebaebe7788fe36751ec4e9
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-05 23:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const outputDiv = document.getElementById('output');
+const output = document.getElementById("output");
+const array = [1, 2, 3, 4];
 
-getNumbers()
-  .then(numbers => {
-    console.log(numbers);
-    return filterOdd(numbers);
-  })
-  .then(evenNumbers => {
-    console.log(evenNumbers);
-    return doubleEven(evenNumbers);
-  })
-  .then(finalNumbers => {
-    console.log(finalNumbers);
-    outputDiv.textContent = finalNumbers.join(',');
-  })
-  .catch(error => {
-    console.error(error);
+function filterEven() {
+  return new Promise((resolve, reject) => {
+    const evenArray = array.filter((e) => e % 2 == 0);
+    setTimeout(() => {
+      output.innerText = `${evenArray}`;
+      resolve(evenArray);
+    }, 1000);
   });
+}
+
+function multiplyByTwo(val) {
+  return new Promise((resolve, reject) => {
+    const result = val.map((e) => e * 2);
+    setTimeout(() => {
+      output.innerText = `${result}`;
+      resolve(result);
+    }, 2000);
+  });
+}
+
+filterEven().then((val) => multiplyByTwo(val));
